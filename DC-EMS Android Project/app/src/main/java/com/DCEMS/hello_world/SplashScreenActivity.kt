bc5ba02405e31b82aefd289b821babe6f217349b
riodelord <42384611+riodelord@users.noreply.github.com>
2020-11-22 14:04:19 +0700

    first push

---
+++ DC-EMS Android Project/app/src/main/java/com/DCEMS/hello_world/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.DCEMS.hello_world
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.os.Handler
+
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
